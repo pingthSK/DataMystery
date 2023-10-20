4824cf993b223895318b47992c41c1823bc02fa3
pingthSK <141994287+pingthSK@users.noreply.github.com>
2023-10-20 14:56:09 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,8 +17,7 @@ function myFunction(){
     d.set('3737', 'goggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggog2481goggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggog1442goggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggog')
     d.set("2481", 'epic super SuS video (Legendey rank )');
     d.set("1442", 'the next conclusion ?????');
-    const primenum = ['2', '3', '5', '7’, ’11’, ’13’, ’17’, ’19’, ’23’, ’29’, ’31’, ’37’, ’41’, ’43’, ’47’, ’53’, ’59’, ’61’, ’67’, ’71’, ’73’, ’79’, ’83’, ’89’, ’97’, ‘101’, ‘103’, ‘107’, ‘109’, ‘113’, ‘127’, ‘131’, ‘137’, ‘139’, ‘149’, ‘151’, ‘157’, ‘163’, ‘167’, ‘173’, ‘179’, ‘181’, ‘191’, ‘193’, ‘197’, ‘199’, ‘211’, ‘223’, ‘227’, ‘229’, ‘233’, ‘239’, ‘241’, ‘251’, ‘257’, ‘263’, ‘269’, ’271’, ‘277’, ‘281’, ‘283’, ‘293’];
-    let test = parseInt(code);
+    const primenum = ['2', '3', '5', '7', '11', '13', '17', '19’, '23', '29', ’31’, ’37’, ’41’, ’43’, ’47’, ’53’, ’59’, ’61’, ’67’, ’71’, ’73’, ’79’, ’83’, ’89’, ’97’, ‘101’, ‘103’, ‘107’, ‘109’, ‘113’, ‘127’, ‘131’, ‘137’, ‘139’, ‘149’, ‘151’, ‘157’, ‘163’, ‘167’, ‘173’, ‘179’, ‘181’, ‘191’, ‘193’, ‘197’, ‘199’, ‘211’, ‘223’, ‘227’, ‘229’, ‘233’, ‘239’, ‘241’, ‘251’, ‘257’, ‘263’, ‘269’, ’271’, ‘277’, ‘281’, ‘283’, ‘293’];
     function linkopen(x){
     const link = document.getElementById('link');
     let clikc = d.get(code);
@@ -27,6 +26,10 @@ function myFunction(){
     link.setAttribute('rel', 'noopener noreferrer');
     link.innerHTML = x;
     }
+    if (code.includes('c')) {
+        let text = "";
+	    for (let x in primenum) 
+    let test = parseInt(code);
     if (test < 500 && test > 300){
         linkopen("YOU GOTTA TYPE! -Putter")
     }else{
